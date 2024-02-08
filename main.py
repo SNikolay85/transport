b7ca76f6d548d82d6ace5ac9ec8c22e397cbd118
nikolay_speshilov <sny_85@mail.ru>
2024-02-08 14:02:26 +0400

    08/02/2024-day1

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 
+from models import create_tables, People, Point, Position, Transport, Car_Fuel, Fuel, Route, Car
+
 DSN = 'postgresql://postgres:postgres@localhost:5432/trans_db'
 engine = sqlalchemy.create_engine(DSN)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+create_tables(engine)
 
 session.close()
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import sqlalchemy as sq
 from sqlalchemy.orm import declarative_base, relationship
+from datetime import datetime
 
 Base = declarative_base()
 
@@ -9,12 +10,13 @@ class Point(Base):
 
     id_point = sq.Column(sq.Integer, primary_key=True)
     name_point = sq.Column(sq.String(length=100))
-    date_of_creation = sq.Column(sq.DateTime)
+    cost = sq.Column(sq.Integer)
 
     def __str__(self):
         return f'Point {self.id_point}: ' \
-               f'({self.name_point}, ' \
-               f'{self.date_of_creation})'
+               f'({self.name_point},' \
+               f'{self.cost})'
+
 
 class Route(Base):
     __tablename__ = 'route'
@@ -32,24 +34,104 @@ class Route(Base):
                f'{self.id_finish_route}, ' \
                f'{self.distance})'
 
+class Fuel(Base):
+    __tablename__ = 'fuel'
+
+    id_fuel = sq.Column(sq.Integer, primary_key=True)
+    name_fuel = sq.Column(sq.String(length=20))
+
+    def __str__(self):
+        return f'Fuel {self.id_fuel}: ' \
+               f'{self.name_fuel})'
+
+
+class Car(Base):
+    __tablename__ = 'car'
+
+    id_car = sq.Column(sq.Integer, primary_key=True)
+    name_car = sq.Column(sq.String(length=40))
+    number_of_car = sq.Column(sq.String(length=20))
+    average_consumption = sq.Column(sq.Integer)
+
+
+    def __str__(self):
+        return f'Car {self.id_car}: ' \
+               f'({self.name_car}, ' \
+               f'{self.number_of_car},' \
+               f'{self.average_consumption},' \
+               f'{self.id_fuel})'
+
+
+class Car_Fuel(Base):
+    __tablename__ = 'car_fuel'
+
+    id_car_fuel = sq.Column(sq.Integer, primary_key=True)
+    id_car = sq.Column(sq.Integer, sq.ForeignKey('car.id_car'), nullable=False)
+    id_fuel = sq.Column(sq.Integer, sq.ForeignKey('fuel.id_fuel'), nullable=False)
+
+    car = relationship(Car, backref='car_fuel')
+    fuel = relationship(Fuel, backref='car_fuel')
+
+class Position(Base):
+    __tablename__ = 'position'
+
+    id_position = sq.Column(sq.Integer, primary_key=True)
+    name_position = sq.Column(sq.String(length=50))
+
+    def __str__(self):
+        return f'Position {self.id_position}: ' \
+               f'{self.name_position},' \
+
+class People(Base):
+    __tablename__ = 'people'
+
+    id_people = sq.Column(sq.Integer, primary_key=True)
+    first_name = sq.Column(sq.String(length=20))
+    last_name = sq.Column(sq.String(length=40))
+    patronymic = sq.Column(sq.String(length=40))
+    id_point = sq.Column(sq.Integer, sq.ForeignKey('point.id_point'), nullable=False)
+    id_position = sq.Column(sq.Integer, sq.ForeignKey('position.id_position'), nullable=False)
+    driving_licence = sq.Column(sq.String(length=30))
+    id_car = sq.Column(sq.Integer, sq.ForeignKey('car.id_car'), nullable=False)
+
+    point = relationship(Point, backref='point')
+    position = relationship(Position, backref='position')
+    car = relationship(Car, backref='people')
+
+    def __str__(self):
+        return f'People {self.id_people}: ' \
+               f'({self.first_name}, ' \
+               f'{self.last_name},' \
+               f'{self.patronymic},' \
+               f'{self.id_point},' \
+               f'{self.id_position},' \
+               f'{self.driving_licence},' \
+               f'{self.id_car})'
 
 class Transport(Base):
     __tablename__ = 'main_table'
 
-    id = sq.Column(sq.Integer, primary_key=True)
-    first_name = sq.Column(sq.String(length=20))
-    last_name = sq.Column(sq.String(length=40))
-    patronymic = sq.Column(sq.String(length=40))
+    id_transport = sq.Column(sq.Integer, primary_key=True)
+    id_people = sq.Column(sq.Integer, sq.ForeignKey('people.id_people'), nullable=False)
     day_of_date = sq.Column(sq.Date)
-    number_of_liters = sq.Column(sq.Integer)
-    number_of_money = sq.Column(sq.Integer)
     id_route = sq.Column(sq.Integer, sq.ForeignKey('route.id_route'), nullable=False)
-    distance = sq.Column(sq.Integer)
     man_to_job = sq.Column(sq.Integer)
     man_with_job = sq.Column(sq.Integer)
+    created_on = sq.Column(sq.DateTime(), default=datetime.now)
+    updated_on = sq.Column(sq.DateTime(), default=datetime.now, onupdate=datetime.now)
 
+    people = relationship(People, backref='main_table')
     route = relationship(Route, backref='main_table')
 
     def __str__(self):
-        return f'Transport {self.day_of_date}: ({self.first_name}, {self.last_name})'
+        return f'Transport {self.id_transport}: ' \
+               f'({self.id_people},' \
+               f'{self.day_of_date},' \
+               f'{self.id_route},' \
+               f'{self.created_on},' \
+               f'{self.updated_on})'
 
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)

279e74477edf90315104fd97fa6cfaea6d222174
Nikolay <sny_85@mail.ru>
2025-03-25 15:27:11 +0400

    2025/03/25

--- utils/point_and_route.py
+++ utils/point_and_route.py
@@ -45,7 +45,7 @@ def description(address1, address2, la1, lon1, la2, lon2):
 
 # п. Мирный, Нефтянников, 62
 name_address_start = 'Завод'
-name_address_finish = 'Самара, Крутые ключи, 33'
+name_address_finish = 'Самара, Магистральная, 64А'
 
 print(get_name_address('53.194529, 50.289504'))
 
